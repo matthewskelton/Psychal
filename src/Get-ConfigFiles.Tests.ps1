56f3eb42360990d63e21c351712703396e834bfc
Matthew Skelton <matthew.skelton@fournines.eu>
2013-01-23 00:24:54 +0000

    Initial tests passing - paths are hardcoded (ugly)

---
+++ src/Get-ConfigFiles.Tests.ps1
@@ -0,0 +1,33 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+    . "$here\$sut"
+
+    Describe "Ensure-NoMatchesFoundForEmptyDir" {
+
+		## Setup -File "$TestDrive\test.txt" "test"
+	
+        It "returns an empty list for no config files but some other files" {
+			$result = Get-ConfigFiles "E:\Downloads\139518\config\none" # "TestDrive:\"
+			$result.Should.Have_Count_Of(0) 
+        }
+		
+    }
+	Describe "Ensure-SingleMatchForSingleFile" {
+		
+		# Setup -File "$TestDrive\test1.txt" "test1"
+	
+			It "returns a one-item list for a single config file" {
+			$result = Get-ConfigFiles "E:\Downloads\139518\config" # "TestDrive:\"
+			$result.Should.Have_Count_Of(1) 
+		}
+    }
+	Describe "Ensure-MatchesForSingleFilesInSubfolders" {
+		
+		# Setup -File "$TestDrive\test1.txt" "test1"
+	
+			It "returns a two-item list for a single config file in parent and subfolders" {
+			$result = Get-ConfigFiles "E:\Downloads\139518\config2" # "TestDrive:\"
+			$result.Should.Have_Count_Of(2) 
+		}
+	}
+	
---
+++ src/Get-ConfigFiles.ps1
@@ -0,0 +1,19 @@
+function Get-ConfigFiles ($path) {
+		$r = @() # ensure IEnumerable
+		$configs = Get-ChildItem -Recurse -Filter *.config $path
+		Write-Host "configs:" $configs
+
+		if ($configs) {
+			if ($configs.Length > 1) {
+					$r = $configs
+				}
+			else {
+				$r = @($configs)
+			}
+		}
+		
+		Write-Host $r.GetType().FullName
+		Write-Host "r.count: " $r.count
+		Write-Host "r.length: " $r.length
+		return ,$r # Force an IEnumerable
+    }
